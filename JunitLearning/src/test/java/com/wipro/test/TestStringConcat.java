e4e3cc8670cb130a9360874988d2c60211e8acdc
user <user@DESKTOP-0UE8K1T.mshome.net>
2026-07-20 21:00:31 +0530

    commit

--- JunitLearning/src/test/java/com/wipro/test/TestStringConcat.java
+++ JunitLearning/src/test/java/com/wipro/test/TestStringConcat.java
@@ -13,8 +13,9 @@ public class TestStringConcat {
 
         DailyTasks obj = new DailyTasks();
 
-        assertEquals("Hello World",
-                obj.doStringConcat("Hello", "World"));
+        String expected = "Hello World";
+        String actual = obj.doStringConcat("Hello", "World");
+
+        assertEquals(expected, actual);
     }
-    
 }
